c47e44e2cb12555b74a8e4e42b929fbdb5d7d776
eltbus <33374178+eltbus@users.noreply.github.com>
2023-11-18 16:23:52 +0100

    Get all parameters

--- main.py
+++ main.py
@@ -3,42 +3,45 @@ from dataclasses import dataclass, field
 from bs4 import BeautifulSoup, Tag
 from typing import List, Iterator
 
-def is_apis_div(element):
+def is_apis_div(element) -> bool:
     return element.name == 'div' and element.get('id', '').endswith("-apis")
 
-def is_definition_div(element):
+def is_definition_div(element) -> bool:
     return element.name == 'div' and element.get('id') == "definitions"
 
-def is_deprecated_div(element):
+def is_deprecated_div(element) -> bool:
     return element.name == 'div' and element.get('id') == "old-api-versions"
 
-def is_resource_container_div(element):
+def is_resource_container_div(element) -> bool:
     return element.name == 'div' and 'resource-container' in element.get('class', [])
 
-def is_inline_definition_container_div(element):
+def is_params_table(element) -> bool:
+    return element.name == 'table' and 'col-md-8' not in element.get('class', [])
+
+def is_inline_definition_container_div(element) -> bool:
     return element.name == 'div' and 'inline-definitions-container' in element.get('class', [])
 
-def is_inline_definition_h3(element):
+def is_inline_definition_h3(element) -> bool:
     return element.name == 'h3' and 'inline-definition' in element.get('class', [])
 
-def is_definition_container_div(element):
+def is_definition_container_div(element) -> bool:
     return element.name == 'div' and 'definition-container' in element.get('class', [])
 
-def is_api_div(element):
+def is_api_div(element) -> bool:
     return (
         is_apis_div(element)
         or is_definition_div(element)
         or is_deprecated_div(element)
     )
 
-def is_resource_div(element):
+def is_resource_div(element) -> bool:
     return (
         is_resource_container_div(element)
         or is_inline_definition_container_div(element)
         or is_definition_container_div(element)
     )
 
-def is_handled_div(element):
+def is_handled_div(element) -> bool:
     return (
         is_api_div(element)
         or is_resource_div(element)
@@ -86,7 +89,15 @@ class Resource:
 
         group_tag =  tag.find('span', class_='g')
         group = group_tag.get_text() if isinstance(group_tag, Tag) and group_tag is not None else ""
-        return cls(kind=kind, group=group)
+
+        parameters = []
+        table_tag = tag.find(is_params_table)
+        if isinstance(table_tag, Tag):
+            tbody_tag = table_tag.find("tbody")
+            if isinstance(tbody_tag, Tag):
+                for row_tag in tbody_tag.find_all("tr"):
+                    parameters.append(Parameter.from_tr_tag(row_tag))
+            return cls(kind=kind, group=group, parameters=parameters)
 
     @classmethod
     def from_inline_definition_container_tag(cls, tag: Tag):
@@ -158,12 +169,15 @@ if __name__ == "__main__":
 
     soup = BeautifulSoup(html_doc, 'html.parser')
 
-    # for api in gen_apis_from_kubernetes_docs(soup):
-    #     print(api.name)
-    #     for resource in api.resources:
-    #         print(f"\t - {resource.kind}")
-    #     print("=" * 120)
+    for api in gen_apis_from_kubernetes_docs(soup):
+        print(api.name)
+        for resource in api.resources:
+            print(f"\t - {resource.kind}")
+            for param in resource.parameters:
+                print(f"\t\t - {param.name}")
+                break
+        print("=" * 120)
 
-    for resource in gen_resources_from_kubernetes_docs(soup):
-        print(resource)
-        pass
+    # for resource in gen_resources_from_kubernetes_docs(soup):
+    #     print(resource)
+    #     pass
