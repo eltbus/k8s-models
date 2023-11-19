1f25e4cdcd0b36bc6247fca11db4a38ca9552c67
eltbus <33374178+eltbus@users.noreply.github.com>
2023-11-19 01:57:58 +0100

    Added workloads API

--- k8s_models/definitions/unknown.py
+++ k8s_models/definitions/unknown.py
@@ -11,3 +11,30 @@ class ScaleSpec(BaseModel):
 
 class ScaleStatus(BaseModel):
     pass
+
+class DaemonSetStatus(BaseModel):
+    pass
+
+class DeploymentStatus(BaseModel):
+    pass
+
+class DeploymentStrategy(BaseModel):
+    pass
+
+class ReplicaSetStatus(BaseModel):
+    pass
+
+class StatefulSetStatus(BaseModel):
+    pass
+
+class PodStatus(BaseModel):
+    pass
+
+class ReplicationControllerStatus(BaseModel):
+    pass
+
+class CronJobStatus(BaseModel):
+    pass
+
+class JobStatus(BaseModel):
+    pass
